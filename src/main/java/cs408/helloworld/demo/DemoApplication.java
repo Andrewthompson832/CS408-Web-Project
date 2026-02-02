3db7044f7c259b533d6f2140af668c1550cab931
Andrew <thompson.drew.4170@gmail.com>
2026-02-02 12:18:00 -0700

    Adding helloworld app

---
+++ src/main/java/cs408/helloworld/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package cs408.helloworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+public static void main(String[] args) {
+	SpringApplication.run(DemoApplication.class, args);
+}
+
+}
---
+++ src/test/java/cs408/helloworld/demo/DemoApplicationTests.java
@@ -0,0 +1,24 @@
+package cs408.helloworld.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testEndpointStatus() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+		}
+
+}
